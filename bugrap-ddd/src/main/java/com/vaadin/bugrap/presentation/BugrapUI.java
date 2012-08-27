b106950fff11bc2a4d8bdc0ac9ba577972c2dfe9
Peter Lehto <peter.lehto@vaadin.com>
2012-08-27 14:53:28 +0300

    Added BugrapUI and modified pom to use latest Vaadin 7 snapshot as well as vaadin-cdi

---
+++ bugrap-ddd/src/main/java/com/vaadin/bugrap/presentation/BugrapUI.java
@@ -0,0 +1,15 @@
+package com.vaadin.bugrap.presentation;
+
+import com.vaadin.cdi.VaadinUI;
+import com.vaadin.terminal.WrappedRequest;
+import com.vaadin.ui.UI;
+
+@VaadinUI
+public class BugrapUI extends UI {
+
+	@Override
+	protected void init(WrappedRequest request) {
+
+	}
+
+}
