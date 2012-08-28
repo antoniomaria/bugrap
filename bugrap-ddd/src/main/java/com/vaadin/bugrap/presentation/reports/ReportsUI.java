6320440bb0cf89915066bdcc2b5cdb5801ec685d
Peter Lehto <peter.lehto@vaadin.com>
2012-08-28 09:00:25 +0300

    Created Reports UI, removed BugrapUI

--- bugrap-ddd/src/main/java/com/vaadin/bugrap/presentation/reports/ReportsUI.java
+++ bugrap-ddd/src/main/java/com/vaadin/bugrap/presentation/reports/ReportsUI.java
@@ -1,14 +1,15 @@
-package com.vaadin.bugrap.presentation;
+package com.vaadin.bugrap.presentation.reports;
 
 import com.vaadin.cdi.VaadinUI;
 import com.vaadin.terminal.WrappedRequest;
 import com.vaadin.ui.UI;
 
-@VaadinUI
-public class BugrapUI extends UI {
+@VaadinUI(mapping = "reports")
+public class ReportsUI extends UI {
 
 	@Override
 	protected void init(WrappedRequest request) {
+		// TODO Auto-generated method stub
 
 	}
 
