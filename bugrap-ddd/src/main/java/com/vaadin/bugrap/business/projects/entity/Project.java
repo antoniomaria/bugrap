f737de87e2827a573469ce9f3137640bcbf405b5
Peter Lehto <peter.lehto@vaadin.com>
2012-08-28 09:23:50 +0300

    Added static project name caption property to Project entity

--- bugrap-ddd/src/main/java/com/vaadin/bugrap/business/projects/entity/Project.java
+++ bugrap-ddd/src/main/java/com/vaadin/bugrap/business/projects/entity/Project.java
@@ -6,18 +6,20 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 
 import com.vaadin.bugrap.business.AbstractEntity;
 import com.vaadin.bugrap.business.users.entity.Reporter;
-import javax.persistence.NamedQuery;
 
 @Entity
-@NamedQuery(name=Project.activeProjects,query="SELECT DISTINCT p FROM ProjectVersion as pv JOIN pv.project as p WHERE pv.closed=:closed")
+@NamedQuery(name = Project.activeProjects, query = "SELECT DISTINCT p FROM ProjectVersion as pv JOIN pv.project as p WHERE pv.closed=:closed")
 public class Project extends AbstractEntity {
-        private static final String PREFIX ="com.vaadin.bugrap.business.projects.entity.Project.";
-        public final static String activeProjects = PREFIX + "activeProjects";
+	private static final String PREFIX = "com.vaadin.bugrap.business.projects.entity.Project.";
+	public final static String activeProjects = PREFIX + "activeProjects";
 	private String name;
+
+	public static String PROJECT_NAME_CAPTION_PROPERTY = "name";
 
 	@ManyToOne(cascade = CascadeType.MERGE)
 	private Reporter manager;
