342b287107535d352f23d0b76fb35a84c5bc4fba
Adam Bien <abien@adam-bien.com>
2012-09-04 21:36:18 +0200

    Firing events implemented in unit tested

--- bugrap-ddd/src/test/java/com/vaadin/bugrap/presentation/reports/ReportsUITest.java
+++ bugrap-ddd/src/test/java/com/vaadin/bugrap/presentation/reports/ReportsUITest.java
@@ -1,6 +1,9 @@
 package com.vaadin.bugrap.presentation.reports;
 
+import com.vaadin.bugrap.presentation.reports.events.ReportBugEvent;
+import com.vaadin.cdi.VaadinContext;
 import com.vaadin.server.WrappedRequest;
+import javax.enterprise.event.Event;
 import javax.inject.Inject;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -10,6 +13,7 @@ import org.jboss.shrinkwrap.api.asset.ByteArrayAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import static org.mockito.Mockito.*;
 
@@ -17,6 +21,7 @@ import static org.mockito.Mockito.*;
  *
  * @author adam-bien.com
  */
+@VaadinContext.VaadinUIScoped
 @RunWith(Arquillian.class)
 public class ReportsUITest {
     
@@ -24,27 +29,38 @@ public class ReportsUITest {
     ReportsUI cut;
     
     @Inject
-    private TopBar topBar;
+    TopBar topBar;
 
     @Inject
-    private ReportsListing reportListing;
+    ReportsListing reportListing;
 
     @Inject
-    private ReportEditor reportEditor;
+    ReportEditor reportEditor;
+    
+    @Inject 
+    private Event<ReportBugEvent> reportBugEvent;
+
 
     @Deployment
     public static JavaArchive createTestArchive() {
         return ShrinkWrap.create(JavaArchive.class, "reports.jar").
-                addClasses(MockProjectRepository.class,ReportsUI.class,TopBar.class,ReportsListing.class,ReportEditor.class).
+                addClasses(ReportsUITest.class,MockProjectRepository.class,ReportsUI.class,
+                TopBar.class,ReportsListing.class,ReportEditor.class).
                 addAsManifestResource(
                 new ByteArrayAsset("<beans/>".getBytes()),
                 ArchivePaths.create("beans.xml"));
     }
     
-    @Test
+    @Before
     public void init(){
         WrappedRequest request = mock(WrappedRequest.class);
         cut.init(request);
+    }
+    
+    @Test
+    public void newBug(){
+        ReportBugEvent rbe = new ReportBugEvent();
+        reportBugEvent.fire(rbe);
     }
 
 }
